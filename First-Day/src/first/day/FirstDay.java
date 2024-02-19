ed57717874e5520c74cd5561b309e33f4406926f
dharam-in <dharamrajyadav6312@gmail.com>
2024-02-19 18:28:58 +0530

    First Day

---
+++ First-Day/src/first/day/FirstDay.java
@@ -0,0 +1,30 @@
+package first.day;
+
+import java.util.*;
+
+public class FirstDay {
+    public static void main(String[] args){
+//        Print in java
+//        System.out.print("India");
+
+//        VARIABLE IN JAVA
+//          int a = 10;
+//          int b = 25;
+//          
+//          int sum = a +b;
+//          System.out.println(sum);
+
+//        INPUT IN JAVA
+          Scanner sc = new Scanner(System.in);
+          System.out.println("Please Enter Your Name");
+          
+          String Name = sc.next();
+          
+          System.out.print(Name);
+            
+
+
+
+    }
+    
+}
