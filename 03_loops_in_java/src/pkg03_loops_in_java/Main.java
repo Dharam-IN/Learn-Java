ac3018ef367abeee1ba337bad304d0e57d7a7a61
dharam-in <dharamrajyadav6312@gmail.com>
2024-02-21 14:03:35 +0530

    Loop Syntex Added

--- 03_loops_in_java/src/pkg03_loops_in_java/Main.java
+++ 03_loops_in_java/src/pkg03_loops_in_java/Main.java
@@ -3,7 +3,30 @@ package pkg03_loops_in_java;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        
+//      FOR LOOP SYNTEX Code:-
+//        for(int count = 0; count < 3; count = count +1){
+//            System.out.println("Hello World " + count);
+//        }
+
+//      PRINT COUNTING FROM ONE TO TEN
+//            for(int count = 0; count < 11; count++){
+//                System.out.println(count);
+//            }
+
+//        WHILE LOOP SYNTEX 
+//            int a = 0; //Initialisation
+//            while(a<10){ //Condition Check
+//                System.out.println(a);
+//                a = a + 1; // Updatation
+//            }
+
+//        DO WHILE SYNTEX
+            int a = 0;
+            do{
+                System.out.println(a);
+                a =  a + 1;
+            }while(a <5);
     }
     
 }
