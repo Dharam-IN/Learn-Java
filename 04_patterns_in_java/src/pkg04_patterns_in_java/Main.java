96c68df42fa226f7bc73c0af688a920fb8f5712b
dharam-in <dharamrajyadav6312@gmail.com>
2024-02-22 13:48:58 +0530

    Basic Pattrern

--- 04_patterns_in_java/src/pkg04_patterns_in_java/Main.java
+++ 04_patterns_in_java/src/pkg04_patterns_in_java/Main.java
@@ -1,20 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pkg04_patterns_in_java;
+import java.util.*;
 
-/**
- *
- * @author aadi
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        for(int a = 0; a<5; a++){
+            for(int b = 0; b<5; b++){
+                System.out.print(" * ");
+            }
+            System.out.print("\n");
+        }
     }
     
 }
