bf66efcb57276b7993c656493e7647371c47ac85
dharam-in <dharamrajyadav6312@gmail.com>
2024-02-22 15:36:03 +0530

    INVERTED HALF PYRAMID (ROTATE BY 180 DEG)

--- 04_patterns_in_java/src/pkg04_patterns_in_java/Main.java
+++ 04_patterns_in_java/src/pkg04_patterns_in_java/Main.java
@@ -14,15 +14,44 @@ public class Main {
 //        }
 
 //        HOLLOW RECTANGLE PATTERN
-            for(int row = 1; row <=4; row++){
-                for(int col = 1; col <= 5; col++){
-                    if(row == 1 || col == 1 || row == 4 || col == 5){
-                        System.out.print("*");
-                    }else{
-                        System.out.print(" ");
-                    }
-                }
-                System.out.print("\n");
+//            for(int row = 1; row <=4; row++){
+//                for(int col = 1; col <= 5; col++){
+//                    if(row == 1 || col == 1 || row == 4 || col == 5){
+//                        System.out.print("*");
+//                    }else{
+//                        System.out.print(" ");
+//                    }
+//                }
+//                System.out.print("\n");
+//            }
+
+//        HALF PYRAMID PATTERN
+//            for(int row = 1; row<=4; row++){
+//                for(int col = 1; col <= row; col++){
+//                    System.out.print(" * ");
+//                }
+//                System.out.print("\n");
+//            }
+
+//        INVERTED HALF PYRAMID
+//            for(int row = 5; row >1; row--){
+//                for(int col = 1; col < row; col++){
+//                    System.out.print("*");
+//                }
+//                System.out.print("\n");
+//            }
+
+//        INVERTED HALF PYRAMID (ROTATE BY 180DEG)
+//            int n = 4;
+//            for(int row = 1; row <= n; row++){
+////              INNER LOOP - SPACE
+//                for(int space = 1; space <= n-row; space++){
+//                    System.out.print(" ");
+//                }
+//                for(int col = 1; col <= row; col++){
+//                    System.out.print("*");
+//                }
+//                System.out.print("\n");
             }
                 
         }
