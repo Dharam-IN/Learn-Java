7c2e12994429239303e30b27be1dae01a388c2d8
dharam-in <dharamrajyadav6312@gmail.com>
2024-02-23 14:56:23 +0530

    Tringle

--- 05_patterns_in_java/src/pkg05_patterns_in_java/Main.java
+++ 05_patterns_in_java/src/pkg05_patterns_in_java/Main.java
@@ -17,23 +17,39 @@ public class Main {
 //         }
 
 //        PALINDROME NUMBER
-            Scanner sc = new Scanner(System.in);
-            int num2 = sc.nextInt();
-            int num = num2;
-            int reverse = 0;
-            
-            while(num2 != 0){
-                int digit = num2 % 10;
-                reverse = (reverse * 10) + digit;
-                num2 = num2 / 10;
-            }
-            
-            System.out.println("Your Number is:- " + num);
-            
-            if(num == reverse){
-                System.out.println("Palindrome Number");
-            }else{
-                System.out.println("Not Palindrome");
+//            Scanner sc = new Scanner(System.in);
+//            int num2 = sc.nextInt();
+//            int num = num2;
+//            int reverse = 0;
+//            
+//            while(num2 != 0){
+//                int digit = num2 % 10;
+//                reverse = (reverse * 10) + digit;
+//                num2 = num2 / 10;
+//            }
+//            
+//            System.out.println("Your Number is:- " + num);
+//            
+//            if(num == reverse){
+//                System.out.println("Palindrome Number");
+//            }else{
+//                System.out.println("Not Palindrome");
+//            }
+
+//        NUMBERS PRINT HALP OF LOOP
+//            for(int a = 1; a <=5; a++){
+//                for(int b = 1; b<= a; b++){
+//                    System.out.print(b);
+//                }
+//                System.out.print("\n");
+//            }
+
+//        INVERTED HALF PYRAMID WITH NUMBERS
+            for(int a = 5; a >= 1; a--){
+                for(int b = 1; b<= a; b++){
+                    System.out.print(b);
+                }
+                System.out.print("\n");
             }
 
     }
