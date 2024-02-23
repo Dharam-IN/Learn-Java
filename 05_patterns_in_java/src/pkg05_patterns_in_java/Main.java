64c60e5fdc627551887b0ba5b3bd04899f26c730
dharam-in <dharamrajyadav6312@gmail.com>
2024-02-23 14:42:45 +0530

    Palindrome Number in Java

--- 05_patterns_in_java/src/pkg05_patterns_in_java/Main.java
+++ 05_patterns_in_java/src/pkg05_patterns_in_java/Main.java
@@ -1,20 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package pkg05_patterns_in_java;
-
-/**
- *
- * @author aadi
- */
+import java.util.*;
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+//         int n = 4;
+//         
+//         for(int row = 1; row <= n; row++){
+//             for(int space = 1; space <= n-row; space++){
+//                 System.out.print(" ");
+//             }
+//             
+//             for(int column = 1; column <= row; column++){
+//                 System.out.print("*");
+//             }
+//             System.out.print("\n");
+//         }
+
+//        PALINDROME NUMBER
+            Scanner sc = new Scanner(System.in);
+            int num2 = sc.nextInt();
+            int num = num2;
+            int reverse = 0;
+            
+            while(num2 != 0){
+                int digit = num2 % 10;
+                reverse = (reverse * 10) + digit;
+                num2 = num2 / 10;
+            }
+            
+            System.out.println("Your Number is:- " + num);
+            
+            if(num == reverse){
+                System.out.println("Palindrome Number");
+            }else{
+                System.out.println("Not Palindrome");
+            }
+
     }
     
 }
