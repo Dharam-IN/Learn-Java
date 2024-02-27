d8285da56978d46e9984d914cddb2d418afd8bb3
dharam-in <dharamrajyadav6312@gmail.com>
2024-02-27 14:26:30 +0530

    Custom Function add

--- 06_JavaFunctions/src/pkg06_javafunctions/Main.java
+++ 06_JavaFunctions/src/pkg06_javafunctions/Main.java
@@ -1,9 +1,20 @@
 
 package pkg06_javafunctions;
 
+import java.util.*;
+
 public class Main {
+    
+    public static void printMyName(String name){
+        System.out.println(name);
+    }
+    
     public static void main(String[] args) {
-        System.out.print("India");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter Your Name:- ");
+        String names = sc.next();
+        
+        printMyName(names);
     }
     
 }
