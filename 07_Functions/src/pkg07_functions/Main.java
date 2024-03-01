1c7baeea9673fb0b89c1d118e23c72f549155279
dharam-in <dharamrajyadav6312@gmail.com>
2024-03-01 13:56:50 +0530

    Exercise Project Add

--- 07_Functions/src/pkg07_functions/Main.java
+++ 07_Functions/src/pkg07_functions/Main.java
@@ -7,7 +7,7 @@ public class Main {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please Enter a number");
+        System.out.println("Please Enter a number:- ");
         int num = sc.nextInt();
         
         int store = 1;
---
+++ 08_Exercise_functions/src/pkg08_exercise_functions/Main.java
@@ -0,0 +1,8 @@
+
+package pkg08_exercise_functions;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("India");
+    }
+    
+}
