78db0391ac0e1b20870008c4928242e511153002
dharam-in <dharamrajyadav6312@gmail.com>
2024-03-28 11:52:34 +0530

    Write a function that takes in the radius as input and returns the circumference of a circle.

--- 09_questions/src/pkg09_questions/Main.java
+++ 09_questions/src/pkg09_questions/Main.java
@@ -1,27 +1,18 @@
 package pkg09_questions;
-
 import java.util.*;
 
 public class Main{
-//    Write a function which takes in 2 numbers and returns the greater of those two.
-        public static void main(String args[]){
-            greater();
-        }
+    public static void main(String[] args){
+        radius();
+    }
+    
+    public static void radius(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter radius value:- ");
+        int radius = sc.nextInt();
         
-        public static void greater(){
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter Your First Number:- ");
-            int num1 = sc.nextInt();
-            System.out.println("Enter Your Second Number:- ");
-            int num2 = sc.nextInt();
-            
-            if(num1 > num2){
-                System.out.println(num1 + " Is greater than " + num2);
-            }else if(num1 == num2){
-                System.out.println(num1 + " and " + num2 + " are equal");
-            }else{
-                System.out.println(num2 + " Is greater than " + num1);
-            }
-        }
+        double circumference = 2 * 3.14 * radius;
         
+        System.out.println(circumference);
+    }
 }
