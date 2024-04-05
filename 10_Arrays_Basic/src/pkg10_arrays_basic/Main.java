62071355c92d2658d8259405553ede2d77796764
dharam-in <dharamrajyadav6312@gmail.com>
2024-04-05 18:08:24 +0530

    Ascending Order

--- 10_Arrays_Basic/src/pkg10_arrays_basic/Main.java
+++ 10_Arrays_Basic/src/pkg10_arrays_basic/Main.java
@@ -1,36 +1,22 @@
 package pkg10_arrays_basic;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-//        Find Largest Number from array
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Entet number you want to array size");
-        int size = sc.nextInt();
-        
-        int[] myarr = new int[size];
-        
-        System.out.println("Enter any value in our array");
-        for(int a = 0; a < myarr.length; a++){
-            myarr[a] = sc.nextInt();
-        }
-        
-        System.out.println("Your Numbers");
-        for(int a = 0; a < myarr.length; a++){
-            System.out.println(myarr[a]);
-        }
-        
-        int largeNo = 0;
-        for(int a = 0; a < myarr.length; a++){
-            if(largeNo < myarr[a]){
-                largeNo = myarr[a];
-            }
-        }
-        
-        System.out.println("Largest Number is in this Array is:- " + largeNo);
-        
-        
+public class Main{
+    public static void main(String[] args){
+       int[] myarr = {5,3,10,1,2};
+       
+       for(int a = 0; a < myarr.length; a++){
+           for(int b = a + 1; b < myarr.length; b++){
+               if(myarr[a] < myarr[b]){
+                   int temp = myarr[a];
+                   myarr[a] = myarr[b];
+                   myarr[b] = temp;
+               }
+           }
+       }
+       
+       for(int a = 0; a < myarr.length; a++){
+           System.out.print(myarr[a] + ", ");
+       }
     }
-    
 }
