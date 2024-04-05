7697d663adfbe6a119a20f6916bc9e3bc0b353da
dharam-in <dharamrajyadav6312@gmail.com>
2024-04-05 09:37:42 +0530

    Find Largest Number from array

--- 10_Arrays_Basic/src/pkg10_arrays_basic/Main.java
+++ 10_Arrays_Basic/src/pkg10_arrays_basic/Main.java
@@ -1,20 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pkg10_arrays_basic;
+import java.util.*;
 
-/**
- *
- * @author aadi
- */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+//        Find Largest Number from array
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entet number you want to array size");
+        int size = sc.nextInt();
+        
+        int[] myarr = new int[size];
+        
+        System.out.println("Enter any value in our array");
+        for(int a = 0; a < myarr.length; a++){
+            myarr[a] = sc.nextInt();
+        }
+        
+        System.out.println("Your Numbers");
+        for(int a = 0; a < myarr.length; a++){
+            System.out.println(myarr[a]);
+        }
+        
+        int largeNo = 0;
+        for(int a = 0; a < myarr.length; a++){
+            if(largeNo < myarr[a]){
+                largeNo = myarr[a];
+            }
+        }
+        
+        System.out.println("Largest Number is in this Array is:- " + largeNo);
+        
+        
     }
     
 }
