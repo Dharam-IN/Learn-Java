318ccab9f59c07e916c69b4bd831e47cf672e29f
dharam-in <dharamrajyadav6312@gmail.com>
2024-04-06 07:18:41 +0530

    Take a matrix as input from the user. Search for a given number x and print the indices at which it occurs.

--- 11_2D_Array/src/pkg11_2d_array/Main.java
+++ 11_2D_Array/src/pkg11_2d_array/Main.java
@@ -1,20 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pkg11_2d_array;
+import java.util.*;
 
-/**
- *
- * @author aadi
- */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+//        Take a matrix as input from the user. Search for a given number x and print the indices at which it occurs.
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size of Rows");
+        int rows = sc.nextInt();
+        System.out.println("Enter Size of Columns");
+        int column = sc.nextInt();
+        
+        int[][] numbers = new int[rows][column];
+        
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < column; j++){
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Your Matrix");
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < column; j++){
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        System.out.println("What do you want to find:- ");
+        int x = sc.nextInt();
+        
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < column; j++){
+                if(x == numbers[i][j]){
+                    System.out.println("Indices of x is: (" + i + ", " + j + ")");
+                }
+            }
+        }
+        
     }
     
 }
