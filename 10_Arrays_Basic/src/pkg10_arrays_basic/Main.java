b6e51920085ca9c685fde109ea913b81602c9884
dharam-in <dharamrajyadav6312@gmail.com>
2024-04-09 05:41:35 +0530

    Take a Matrix as Input from the user. search for a given number x and print the indices at which it occurs

--- 10_Arrays_Basic/src/pkg10_arrays_basic/Main.java
+++ 10_Arrays_Basic/src/pkg10_arrays_basic/Main.java
@@ -3,26 +3,38 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args){
-//        Reverse Array
+        Scanner sc = new Scanner(System.in);
         
-        int[] myarr = {1,2,5,4,6,3,2};
+        System.out.println("Enter Array Row size:- ");
+        int row = sc.nextInt();
+        System.out.println("Enter Array Column size:- ");
+        int col = sc.nextInt();
         
-        System.out.println("Your Array:- ");
-        for(int i = 0; i < myarr.length; i++){
-            System.out.print(myarr[i] + ", ");
+        int[][] numbers = new int[row][col];
+        
+        System.out.println("Enter Array Values:- ");
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                numbers[i][j] = sc.nextInt();
+            }
         }
         
-        System.out.println("Reverse Array:-");
-        
-        int[] newarr = new int[myarr.length];
-        
-        for(int i = 0; i < myarr.length; i++){
-            int temp = myarr.length;
-            newarr[i] = myarr[temp - 1 - i];
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                System.out.print(numbers[i][j] + ", ");
+            }
+            System.out.println();
         }
         
-        for(int i = 0; i < newarr.length; i++){
-            System.out.print(newarr[i] + ", ");
+        System.out.println("Enter a number you Want to find that's indeces:- ");
+        int x = sc.nextInt();
+        
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(numbers[i][j] == x){
+                    System.out.print(i + ", " + j);
+                }
+            }
         }
     }
 }
