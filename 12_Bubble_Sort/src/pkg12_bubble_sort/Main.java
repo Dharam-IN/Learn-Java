933a9dbd27277e496a2301cf0df8d31fc60e54f5
dharam-in <dharamrajyadav6312@gmail.com>
2024-04-10 06:43:11 +0530

    Bubble Sort

--- 12_Bubble_Sort/src/pkg12_bubble_sort/Main.java
+++ 12_Bubble_Sort/src/pkg12_bubble_sort/Main.java
@@ -1,20 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pkg12_bubble_sort;
-
-/**
- *
- * @author aadi
- */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[] myarr = {7,8,3,1,2};
+        
+        for(int i = 0; i < myarr.length - 1; i++){
+            for(int j = 0; j < myarr.length - i -1; j++){
+                if(myarr[j] > myarr[j+1]){
+                    int temp = myarr[j];
+                    myarr[j] = myarr[j+1];
+                    myarr[j+1] = temp;
+                }
+            }
+        }
+        
+        for(int i = 0; i < myarr.length; i++){
+            System.out.print(myarr[i]);
+        }
     }
     
 }
