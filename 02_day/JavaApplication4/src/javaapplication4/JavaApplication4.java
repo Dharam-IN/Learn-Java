146971f9b8da9fba5a0facf302556272d2e79d4b
dharam-in <dharamdotin@gmail.com>
2024-07-15 17:09:31 +0530

    second Day

---
+++ 02_day/JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,17 @@
+
+package javaapplication4;
+
+import java.util.Scanner;
+
+public class JavaApplication4 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        System.out.println("Please enter your age: ");
+//        float age = input.nextFloat();
+
+        int num = (int)(67.155445f);
+        System.out.println(num);
+    }
+    
+}
